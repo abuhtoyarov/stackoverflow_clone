cdeb9cbf9a82d19a1b2ce4c3a711922846bed6e5
Dmitriy Ivliev <rootkit89@gmail.com>
2015-03-14 17:57:14 +0400

    Add valid tests for answer controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
-  let(:answer) { create(:answer) }
+  let(:answer) { create(:answer, question: question) }
 
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'saves the new post to database' do
+        # this genereate expected request i.e.:
+        # {:action=>"create", :answer=>{:body=>"MyString"},
+        #   :controller=>"answers", :question_id=>"193"}
+        expect { post :create, answer: attributes_for(:answer), question_id: question }.
+          to change(question.answers, :count).by(1)
+      end
+      it 'redirect to question #show' do
+        post :create, answer: attributes_for(:answer), question_id: question
+        expect(response).to redirect_to(:question)
       end
     end
 
