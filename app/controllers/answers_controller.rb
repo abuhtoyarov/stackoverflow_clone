61d73fd5cdc67d5a584f7c7161fa3838b957a52d
Dmitriy Ivliev <rootkit89@gmail.com>
2015-03-16 19:39:06 +0400

    small fix for query

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -7,7 +7,7 @@ class AnswersController < ApplicationController
       redirect_to @question
     else
       #render DONT execute any code in the action so we need to assign @answers
-      @answers = Question.find(params[:question_id]).answers
+      @answers = @question.answers.reset
       render 'questions/show'
     end
   end
