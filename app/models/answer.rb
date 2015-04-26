1e01113f0cb4262a6bda4f3e6c76aecc4d5cd1b4
Dmitriy Ivliev <rootkit89@gmail.com>
2015-04-26 23:07:39 +0400

    add delete file to answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,7 +5,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
 
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 
   validates :body, :user_id, presence: true
 
